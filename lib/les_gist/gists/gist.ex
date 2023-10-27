a115f8b23fc7d2758de3f4d1936edfb1614afe10
Mwai Joseph <joesoftmwai@gmail.com>
2023-10-27 13:09:41 +0300

    chore: disabled button while submiting a form

--- lib/les_gist/gists/gist.ex
+++ lib/les_gist/gists/gist.ex
@@ -16,6 +16,6 @@ defmodule LesGist.Gists.Gist do
   def changeset(gist, attrs) do
     gist
     |> cast(attrs, [:name, :description, :markup_text, :user_id])
-    |> validate_required([:name, :description, :user_id])
+    |> validate_required([:name, :description, :markup_text, :user_id])
   end
 end
